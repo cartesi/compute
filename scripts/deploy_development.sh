c7b107244a6f3296fdfff9a9b737cb417f6a8730
Gabriel Barros <gabriel.barros@cartesi.io>
2020-07-28 17:48:47 -0300

    Fixes wrong result response on getResult

---
+++ scripts/deploy_development.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# this script will compile and migrate not only this project contracts, but also all dependent contracts, in the correct order
+
+cd ..
+# remove build directory to do a clean build
+rm ./build/ -rf
+root=$PWD
+
+cd ./node_modules/@cartesi/util && npx truffle migrate --network development && cd $root
+cd ./node_modules/@cartesi/arbitration && npx truffle migrate --network development && cd $root
+cd ./node_modules/@cartesi/machine-solidity-step && npx truffle migrate --network development && cd $root
+cd ./node_modules/@cartesi/logger && npx truffle migrate --network development && cd $root
+npx truffle migrate --network development
