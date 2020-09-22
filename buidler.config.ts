aec45af376ec80a190c8a5873445225032f1b6f8
Milton Jonathan <milton.jonathan@cartesi.io>
2020-09-22 08:20:18 -0300

    Updating/fixing buidler setup

--- buidler.config.ts
+++ buidler.config.ts
@@ -1,72 +1,175 @@
-import { usePlugin } from "@nomiclabs/buidler/config";
+import fs from "fs";
+import { Wallet } from "@ethersproject/wallet";
+import { BuidlerConfig, task, usePlugin } from "@nomiclabs/buidler/config";
+import { HttpNetworkConfig } from "@nomiclabs/buidler/types";
 
-const project = process.env.PROJECT_ID;
-const mnemonic = process.env.MNEMONIC || "placeholder";
-
-const network = (name:string, network_id:number, url=`https://${name}.infura.io/v3/${project}`) => ({
-  url,
-  accounts: {
-    mnemonic,
-    count: 100,
-  },
-  chainId: network_id,
-});
-
-usePlugin("@nomiclabs/buidler-solpp");
+usePlugin("@nomiclabs/buidler-ethers");
 usePlugin("@nomiclabs/buidler-waffle");
 usePlugin("@nodefactory/buidler-typechain");
-usePlugin('solidity-coverage')
 usePlugin("buidler-deploy");
 
-module.exports = {
-  defaultNetwork: "buidlerevm",
+usePlugin("@nomiclabs/buidler-solpp");
+usePlugin("solidity-coverage");
+
+// This is a sample Buidler task. To learn how to create your own go to
+// https://buidler.dev/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, bre) => {
+  const accounts = await bre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(await account.getAddress());
+  }
+});
+
+// read MNEMONIC from file or from env variable
+let mnemonic = process.env.MNEMONIC;
+try {
+  mnemonic = fs
+    .readFileSync(process.env.MNEMONIC_PATH || ".mnemonic")
+    .toString();
+} catch (e) {}
+
+// create a Buidler EVM account array from mnemonic
+const mnemonicAccounts = (n = 10) => {
+  return mnemonic
+    ? Array.from(Array(n).keys()).map((i) => {
+        const wallet = Wallet.fromMnemonic(
+          mnemonic as string,
+          `m/44'/60'/0'/0/${i}`
+        );
+        return {
+          privateKey: wallet.privateKey,
+          balance: "1000000000000000000000",
+        };
+      })
+    : undefined;
+};
+
+const infuraNetwork = (
+  network: string,
+  chainId?: number,
+  gas?: number
+): HttpNetworkConfig => {
+  return {
+    url: `https://${network}.infura.io/v3/${process.env.PROJECT_ID}`,
+    chainId,
+    gas,
+    accounts: mnemonic ? { mnemonic } : undefined,
+  };
+};
+
+const config: BuidlerConfig = {
   networks: {
-    development: {
-      url: "http://127.0.0.1:8545"
-    },
-    buidlerevm: {
-      // See its defaults
+    buidlerevm: mnemonic ? { accounts: mnemonicAccounts() } : {},
+    localhost: {
+      url: "http://localhost:8545",
+      accounts: mnemonic ? { mnemonic } : undefined,
     },
     coverage: {
-      url: "http://127.0.0.1:8555", // <-- If you change this, also set the port option in .solcover.js.
+      url: "http://localhost:8555", // <-- If you change this, also set the port option in .solcover.js.
       // chainId: "*", // not set means it's ignored
       gas: 0xfffffffffff, // <-- Use this high gas value
-      gasPrice: 0x01      // <-- Use this low gas price
+      gasPrice: 0x01, // <-- Use this low gas price
     },
-    ropsten: network("ropsten", 3),
-    kovan: network("kovan", 42),
-    rinkeby: network("rinkeby", 4),
-    matic_testnet: network("matic_testnet", 15001, 'https://testnetv3.matic.network'),
-    ganache: {
-      url: "http://ganache:8545"
+    ropsten: infuraNetwork("ropsten", 3, 6283185),
+    rinkeby: infuraNetwork("rinkeby", 4, 6283185),
+    kovan: infuraNetwork("kovan", 42, 6283185),
+    goerli: infuraNetwork("goerli", 5, 6283185),
+    matic_testnet: {
+      url: "https://rpc-mumbai.matic.today",
+      chainId: 80001,
+      accounts: mnemonic ? { mnemonic } : undefined,
+    },
+    bsc_testnet: {
+      url: "https://data-seed-prebsc-1-s1.binance.org:8545",
+      chainId: 97,
+      accounts: mnemonic ? { mnemonic } : undefined,
     },
   },
-  namedAccounts: {
-    deployer: {
-        default: 0
+  solc: {
+    version: "0.7.1",
+    optimizer: {
+      enabled: true,
     },
-    claimer: {
-        default: 0
-    },
-    challenger: {
-        default: 1
+  },
+  paths: {
+    artifacts: "artifacts",
+    deploy: "deploy",
+    deployments: "deployments",
+  },
+  external: {
+    artifacts: [
+      "node_modules/@cartesi/util/artifacts",
+      "node_modules/@cartesi/arbitration/artifacts",
+      "node_modules/@cartesi/logger/artifacts",
+      "node_modules/@cartesi/machine-solidity-step/artifacts",
+    ],
+    deployments: {
+      localhost: [
+        "node_modules/@cartesi/util/deployments/localhost",
+        "node_modules/@cartesi/arbitration/deployments/localhost",
+        "node_modules/@cartesi/logger/deployments/localhost",
+        "node_modules/@cartesi/machine-solidity-step/deployments/localhost",
+      ],
+      ropsten: [
+        "node_modules/@cartesi/util/deployments/ropsten",
+        "node_modules/@cartesi/arbitration/deployments/ropsten",
+        "node_modules/@cartesi/logger/deployments/ropsten",
+        "node_modules/@cartesi/machine-solidity-step/deployments/ropsten",
+      ],
+      rinkeby: [
+        "node_modules/@cartesi/util/deployments/rinkeby",
+        "node_modules/@cartesi/arbitration/deployments/rinkeby",
+        "node_modules/@cartesi/logger/deployments/rinkeby",
+        "node_modules/@cartesi/machine-solidity-step/deployments/rinkeby",
+      ],
+      kovan: [
+        "node_modules/@cartesi/util/deployments/kovan",
+        "node_modules/@cartesi/arbitration/deployments/kovan",
+        "node_modules/@cartesi/logger/deployments/kovan",
+        "node_modules/@cartesi/machine-solidity-step/deployments/kovan",
+      ],
+      goerli: [
+        "node_modules/@cartesi/util/deployments/goerli",
+        "node_modules/@cartesi/arbitration/deployments/goerli",
+        "node_modules/@cartesi/logger/deployments/goerli",
+        "node_modules/@cartesi/machine-solidity-step/deployments/goerli",
+      ],
+      matic_testnet: [
+        "node_modules/@cartesi/util/deployments/matic_testnet",
+        "node_modules/@cartesi/arbitration/deployments/matic_testnet",
+        "node_modules/@cartesi/logger/deployments/matic_testnet",
+        "node_modules/@cartesi/machine-solidity-step/deployments/matic_testnet",
+      ],
+      bsc_testnet: [
+        "node_modules/@cartesi/util/deployments/bsc_testnet",
+        "node_modules/@cartesi/arbitration/deployments/bsc_testnet",
+        "node_modules/@cartesi/logger/deployments/bsc_testnet",
+        "node_modules/@cartesi/machine-solidity-step/deployments/bsc_testnet",
+      ],
     },
   },
   solpp: {
     defs: {
-      BUILD_TEST: process.argv.includes('test') || process.argv.includes('coverage'),
-    }
+      BUILD_TEST:
+        process.argv.includes("test") || process.argv.includes("coverage"),
+    },
   },
   typechain: {
     outDir: "src/types",
     target: "ethers-v5",
   },
-  solc: {
-    version: "0.5.16",
-     // See the solidity docs for advice about optimization and evmVersion
-     optimizer: {
-      enabled: true,
-      runs: 110,
+  namedAccounts: {
+    deployer: {
+      default: 0,
+    },
+    alice: {
+      default: 0,
+    },
+    proxy: {
+      default: 1,
     },
   },
 };
+
+export default config;
