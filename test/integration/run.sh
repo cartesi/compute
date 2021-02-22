baa189b5ec89f57be61ff46a77828d9c2cee92e4
Gabriel Barros <gabriel.barros@cartesi.io>
2021-02-22 19:48:44 +0000

    Add integration test base scripts

---
+++ test/integration/run.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+FULL_PATH=$(dirname $(realpath $0))
+DESCARTES_DIR=$(dirname $(dirname $FULL_PATH))
+
+
+echo $DESCARTES_DIR
+echo $FULL_PATH
+
+cd $DESCARTES_DIR;
+
+wait-for-url() {
+    echo "wait-for-url $1"
+    timeout -s TERM 90 bash -c \
+    'while [[ "$(curl -s -o /dev/null -L -w ''%{http_code}'' ${0})" != "200" ]];\
+    do echo "Waiting for ${0}" && sleep 2;\
+    done' ${1}
+    echo "OK!"
+    curl -I $1
+}
+
+jinja2 -D num_players=2 docker-compose-template.yml | docker-compose -f - up --build --no-color >& logs.txt&
+wait-for-url http://localhost:8545
+
+
+
+./scripts/helloworld/build-cartesi-machine.sh ./machines
+./scripts/calculator/build-cartesi-machine.sh ./machines
+./scripts/ipfs/run.sh
+
+
+npx hardhat run --network localhost --no-compile ./scripts/helloworld/instantiate.ts
+npx hardhat run --network localhost --no-compile ./scripts/calculator/instantiate.ts
+npx hardhat run --network localhost --no-compile ./scripts/calculator/instantiate-logger.ts
+
+npx hardhat run --network localhost --no-compile ./test/integration/wait-results.ts
+
+
+jinja2 -D num_players=2 docker-compose-template.yml | docker-compose -f - down -v
---
+++ test/integration/wait-results.ts
@@ -0,0 +1,103 @@
+/**
+ * HelloWorld instantiate
+ *
+ * Basic usage
+ * - npx hardhat run --network localhost --no-compile helloworld/instantiate.ts
+ */
+import hre from "hardhat";
+
+async function sleep(seconds: number): Promise<any> {
+  return new Promise((resolve) => {
+    setTimeout(resolve, seconds * 1000);
+  });
+}
+
+async function main() {
+  const failedTests: string[] = [];
+  const { ethers, getNamedAccounts } = hre;
+  const { Descartes } = await hre.deployments.all();
+
+  const { alice, bob, charlie, dave } = await getNamedAccounts();
+
+  let num_peers = 2;
+  if (process.env.num_peers) {
+    num_peers = Number.parseInt(process.env.num_peers);
+  }
+  const peers = [alice, bob, charlie, dave].slice(0, num_peers);
+
+  // retrieves deployed Descartes instance based on its address
+  const descartes = await ethers.getContractAt(
+    "Descartes",
+    Descartes.address
+  );
+
+  console.log("");
+  console.log(`Grabbing information on deployed Descartes\n`);
+
+  const filter = descartes.filters.DescartesCreated();
+
+  const events = await descartes.queryFilter(filter, 0, 'latest');
+
+  console.log(`Found ${events.length} Descartes instances running`);
+  const active_idx: number[] = [];
+  events.forEach((e) => {
+    const arg = e.args || { _index: 0 };
+    active_idx.push(arg._index.toNumber());
+  })
+
+  while (active_idx.length > 0) {
+    await Promise.all(active_idx.map(async (instance, idx) => {
+      const state = await descartes.getCurrentState(instance);
+      const parsedState = ethers.utils.parseBytes32String(state);
+      console.log(`Instance ${instance} is at state ${parsedState}`);
+
+      switch (parsedState) {
+        case "ConsensusResult":
+          active_idx.splice(idx, 1);
+          break;
+        case "ChallengerWon":
+          active_idx.splice(idx, 1);
+          failedTests.push(`Descartes test at index ${instance} has failed. Finished at ${parsedState} state.`);
+          break;
+        case "ClaimerWon":
+          active_idx.splice(idx, 1);
+          failedTests.push(`Descartes test at index ${instance} has failed. Finished at ${parsedState} state.`);
+          break;
+        case "ProviderMissedDeadline":
+          active_idx.splice(idx, 1);
+          failedTests.push(`Descartes test at index ${instance} has failed. Finished at ${parsedState} state.`);
+          break;
+        case "ClaimerMissedDeadline":
+          active_idx.splice(idx, 1);
+          failedTests.push(`Descartes test at index ${instance} has failed. Finished at ${parsedState} state.`);
+          break;
+      }
+
+    }));
+    console.log('------------- sleeping 20s');
+    await sleep(20);
+  }
+  if (failedTests.length > 0) {
+    throw (`Some tests failed. Here is the compiled messages: ${JSON.stringify(failedTests, null, 2)}`);
+  }
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+
+const timeout = new Promise((_, rejects) => {
+  let time = 30 * 1000 * 60; // 30min
+  if (process.env.timeout) {
+    time = Number.parseInt(process.env.timeout, 10) * 1000 * 60;
+  }
+  setTimeout(() => {
+    rejects(`The test has timedout after waiting for ${time / (1000 * 60)} min. Check the logs.`);
+  }, time);
+})
+
+Promise.race([main(), timeout])
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
