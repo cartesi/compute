fe8d765419b81f5baa78cbf1649e37033edcb156
Milton Jonathan <milton.jonathan@cartesi.io>
2021-03-06 18:23:05 -0300

    Integration test printing instance status in order

--- test/integration/wait-results.ts
+++ test/integration/wait-results.ts
@@ -7,97 +7,116 @@
 import hre from "hardhat";
 
 async function sleep(seconds: number): Promise<any> {
-  return new Promise((resolve) => {
-    setTimeout(resolve, seconds * 1000);
-  });
+    return new Promise((resolve) => {
+        setTimeout(resolve, seconds * 1000);
+    });
 }
 
 async function main() {
-  const failedTests: string[] = [];
-  const { ethers, getNamedAccounts } = hre;
-  const { Descartes } = await hre.deployments.all();
+    const failedTests: string[] = [];
+    const { ethers, getNamedAccounts } = hre;
+    const { Descartes } = await hre.deployments.all();
 
-  const { alice, bob, charlie, dave } = await getNamedAccounts();
+    const { alice, bob, charlie, dave } = await getNamedAccounts();
 
-  let num_peers = 2;
-  if (process.env.num_peers) {
-    num_peers = Number.parseInt(process.env.num_peers);
-  }
-  const peers = [alice, bob, charlie, dave].slice(0, num_peers);
+    let num_peers = 2;
+    if (process.env.num_peers) {
+        num_peers = Number.parseInt(process.env.num_peers);
+    }
+    const peers = [alice, bob, charlie, dave].slice(0, num_peers);
 
-  // retrieves deployed Descartes instance based on its address
-  const descartes = await ethers.getContractAt(
-    "Descartes",
-    Descartes.address
-  );
+    // retrieves deployed Descartes instance based on its address
+    const descartes = await ethers.getContractAt(
+        "Descartes",
+        Descartes.address
+    );
 
-  console.log("");
-  console.log(`Grabbing information on deployed Descartes\n`);
+    console.log("");
+    console.log(`Grabbing information on deployed Descartes\n`);
 
-  const filter = descartes.filters.DescartesCreated();
+    const filter = descartes.filters.DescartesCreated();
 
-  const events = await descartes.queryFilter(filter, 0, 'latest');
+    const events = await descartes.queryFilter(filter, 0, "latest");
 
-  console.log(`Found ${events.length} Descartes instances running`);
-  const active_idx: number[] = [];
-  events.forEach((e) => {
-    const arg = e.args || { _index: 0 };
-    active_idx.push(arg._index.toNumber());
-  })
+    console.log(`Found ${events.length} Descartes instances running`);
+    const activeInstances: number[] = [];
+    events.forEach((e) => {
+        const arg = e.args || { _index: 0 };
+        activeInstances.push(arg._index.toNumber());
+    });
 
-  while (active_idx.length > 0) {
-    await Promise.all(active_idx.map(async (instance, idx) => {
-      const state = await descartes.getCurrentState(instance);
-      const parsedState = ethers.utils.parseBytes32String(state);
-      console.log(`Instance ${instance} is at state ${parsedState}`);
+    while (activeInstances.length > 0) {
+        let i = 0;
+        while (i < activeInstances.length) {
+            const instance = activeInstances[i];
+            const state = await descartes.getCurrentState(instance);
+            const parsedState = ethers.utils.parseBytes32String(state);
+            console.log(`Instance ${instance} is at state ${parsedState}`);
 
-      switch (parsedState) {
-        case "ConsensusResult":
-          active_idx.splice(idx, 1);
-          break;
-        case "ChallengerWon":
-          active_idx.splice(idx, 1);
-          failedTests.push(`Descartes test at index ${instance} has failed. Finished at ${parsedState} state.`);
-          break;
-        case "ClaimerWon":
-          active_idx.splice(idx, 1);
-          failedTests.push(`Descartes test at index ${instance} has failed. Finished at ${parsedState} state.`);
-          break;
-        case "ProviderMissedDeadline":
-          active_idx.splice(idx, 1);
-          failedTests.push(`Descartes test at index ${instance} has failed. Finished at ${parsedState} state.`);
-          break;
-        case "ClaimerMissedDeadline":
-          active_idx.splice(idx, 1);
-          failedTests.push(`Descartes test at index ${instance} has failed. Finished at ${parsedState} state.`);
-          break;
-      }
-
-    }));
-    console.log('------------- sleeping 20s');
-    await sleep(20);
-  }
-  if (failedTests.length > 0) {
-    throw (`Some tests failed. Here is the compiled messages: ${JSON.stringify(failedTests, null, 2)}`);
-  }
+            switch (parsedState) {
+                case "ConsensusResult":
+                    activeInstances.splice(i, 1);
+                    break;
+                case "ChallengerWon":
+                    activeInstances.splice(i, 1);
+                    failedTests.push(
+                        `Descartes test at index ${instance} has failed. Finished at ${parsedState} state.`
+                    );
+                    break;
+                case "ClaimerWon":
+                    activeInstances.splice(i, 1);
+                    failedTests.push(
+                        `Descartes test at index ${instance} has failed. Finished at ${parsedState} state.`
+                    );
+                    break;
+                case "ProviderMissedDeadline":
+                    activeInstances.splice(i, 1);
+                    failedTests.push(
+                        `Descartes test at index ${instance} has failed. Finished at ${parsedState} state.`
+                    );
+                    break;
+                case "ClaimerMissedDeadline":
+                    activeInstances.splice(i, 1);
+                    failedTests.push(
+                        `Descartes test at index ${instance} has failed. Finished at ${parsedState} state.`
+                    );
+                    break;
+                default:
+                    i++;
+            }
+        }
+        console.log("------------- sleeping 20s");
+        await sleep(20);
+    }
+    if (failedTests.length > 0) {
+        throw `Some tests failed. Here is the compiled messages: ${JSON.stringify(
+            failedTests,
+            null,
+            2
+        )}`;
+    }
 }
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
 
 const timeout = new Promise((_, rejects) => {
-  let time = 30 * 1000 * 60; // 30min
-  if (process.env.timeout) {
-    time = Number.parseInt(process.env.timeout, 10) * 1000 * 60;
-  }
-  setTimeout(() => {
-    rejects(`The test has timedout after waiting for ${time / (1000 * 60)} min. Check the logs.`);
-  }, time);
-})
+    let time = 30 * 1000 * 60; // 30min
+    if (process.env.timeout) {
+        time = Number.parseInt(process.env.timeout, 10) * 1000 * 60;
+    }
+    setTimeout(() => {
+        rejects(
+            `The test has timedout after waiting for ${
+                time / (1000 * 60)
+            } min. Check the logs.`
+        );
+    }, time);
+});
 
 Promise.race([main(), timeout])
-  .then(() => process.exit(0))
-  .catch((error) => {
-    console.error(error);
-    process.exit(1);
-  });
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
