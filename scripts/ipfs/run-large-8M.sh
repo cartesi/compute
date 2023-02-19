b62b655e3ae063e3af9e00eee9601fa0e41fbdb3
Carsten Munk <carsten@zippie.com>
2023-02-19 08:14:15 +0100

    Fix typo in 8M ipfs message
    Signed-off-by: Carsten Munk <carsten@zippie.com>

--- scripts/ipfs/run-large-8M.sh
+++ scripts/ipfs/run-large-8M.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Using large file: 8MB (DRIVE_LOG2_SIZE = 20)"
+echo "Using large file: 8MB (DRIVE_LOG2_SIZE = 23)"
 export DRIVE_LOG2_SIZE=23
 
 FULL_PATH=$(dirname $(realpath $0))
